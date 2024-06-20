94c28a05a464081f0a79df972d738092e47ccd71
Ayushtawar13 <aayushtanwar2003@gmail.com>
2024-06-20 22:44:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  let a= document.getElementById('add')
+  let s= document.getElementById('sub')
+  let [counter , setCounter] = useState(15);
+
+  function addCounter(){
+     if(counter >= 20)
+     {
+       alert("cannot accept value more than 20");
+     }
+     else{
+      setCounter(counter+1);
+     }
+  }
+
+  function subCounter(){
+    if(counter <= 0)
+    {
+      alert("cannot accept value less than 0");
+    }
+    else{
+     setCounter(counter-1);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <>
+      
+      <h1>The counter App</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={addCounter} value={20} >Add Value {counter}</button>
+
+      <button onClick={subCounter}>Remove Value {counter}</button>
+
+     </>
+  )
 }
 
 export default App;
